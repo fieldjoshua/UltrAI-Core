95ab9f792b517b8c316133c6590b8a5d5642850b
fieldjoshua <jfield@forresterfield.com>
2025-04-06 15:30:16 -0700

    Setup cloud deployment with Vercel and Render configurations

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css'
 
+// Simple, direct rendering without StrictMode
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 ) 
