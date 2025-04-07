af2b6c35656fed4fda97e26308318e04a53a5e47
fieldjoshua <jfield@forresterfield.com>
2025-04-07 04:17:21 -0400

    Add Python and ESLint configurations

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2022: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'react-hooks'],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        // Error prevention
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+        // React specific
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+    },
+    ignorePatterns: [
+        'dist/',
+        'build/',
+        'node_modules/',
+        '*.config.js',
+        'vite.config.ts',
+    ],
+};
