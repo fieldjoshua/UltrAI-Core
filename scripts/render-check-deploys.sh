62458c6441e8916cb8c28eadcbd7847625359b22
fieldjoshua <jfield@forresterfield.com>
2025-09-29 21:37:11 -0700

    chore: remove duplicate services from monitoring

--- scripts/render-check-deploys.sh
+++ scripts/render-check-deploys.sh
@@ -3,13 +3,9 @@
 RENDER_API_KEY="rnd_NfrFXrFHdSs0kU2LfFfaWPkN6lzH"
 
 SERVICES="ultrai-prod-api:srv-d2qdi27diees73cscfb0
-UltrAI:srv-d0k71jt6ubrc73b1be40
-ultrai-core:srv-d0l9lr56ubrc73bt2bh0
 ultrai-staging-api:srv-d2qkdaadbo4c73c876rg
-ultrai-frontend:srv-d2nmsm0gjchc73dl1dd0
 ultrai-prod-ui:srv-d2qdepje5dus73btqt10
 ultrai-demo:srv-d2qcrifdiees73crqpkg
-ultrai-demo-ui:srv-d2qkepp5pdvs738aq9m0
 ultrai-staging-ui:srv-d2qkelp5pdvs738aq6mg"
 
 echo "Checking deploy status for all services..."
